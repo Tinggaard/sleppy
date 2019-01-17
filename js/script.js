48423caea5aaac594214b3a3b9c648c1b727ad4f
Jens Tinggaard <tinggaard@yahoo.com>
2019-01-17 09:22:24 +0100

    Final commit

--- js/script.js
+++ js/script.js
@@ -12,26 +12,54 @@ $(document).ready(function () {
 
 });
 
+
+
 // Load google charts
 google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 
 // Draw the chart and set the chart values
 function drawChart() {
-	var data = google.visualization.arrayToDataTable([
+	// Table 1
+	var data1 = google.visualization.arrayToDataTable([
 		['Task', 'Timer om dagen'],
 		['Skole', 8],
 		['Spiser', 2],
-		['Træning', 4],
-		['Gym', 2],
-		['Søvn', 8]
+		['Træning', 3],
+		['Søvn', 7.5],
+		['Lektier', 3.5]
 	]);
 
 	// Optional; add a title and set the width and height of the chart
-	var options = { 'title': 'Din gennemsnitsdag' };
+	var options1 = { 'title': 'Din gennemsnitsdag'};
 
 	// Display the chart inside the <div> element with id="piechart"
 	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-	chart.draw(data, options);
-}
+	chart.draw(data1, options1);
 
+
+	
+	// Table 2
+	var data2 = google.visualization.arrayToDataTable([
+		['Dag siden start', 'Dig', 'Gennemsnit'],
+		['Dag 1', 7, 6.5],
+		['Dag 2', 7, 7],
+		['Dag 3', 7.5, 6.5],
+		['Dag 4', 7, 6.5],
+		['Dag 5', 8, 7],
+		['Dag 6', 7.5, 7]
+	]);
+
+	var options2 = {
+		title: 'Din søvnrytme',
+		curveType: 'function',
+		legend: { position: 'bottom' },
+		vAxis: { title: 'Timer' }
+	};
+
+	var chart = new google.visualization.LineChart(document.getElementById('linechart'));
+
+	chart.draw(data2, options2);
+
+
+}
